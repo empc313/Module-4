b356c28b7080b0c1811fa44eff10e1f1ffee639a
Emma <cemma313@gmail.com>
2023-02-15 13:53:45 -0700

    updated html and js

---
+++ script.js
@@ -0,0 +1,13 @@
+//elements needed
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var initialsEl = document.querySelector("#initials");
+
+//Questions
+var questions = [
+  { title: "What", choices: ["1", "2", "3", "4"], answer: "1" },
+  { title: "What", choices: ["1", "2", "3", "4"], answer: "1" },
+  { title: "What", choices: ["1", "2", "3", "4"], answer: "1" },
+];
