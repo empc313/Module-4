7c1034340d23c97d03f8a9545e483034b64cfe7e
Emma <cemma313@gmail.com>
2023-02-15 14:09:02 -0700

    updated js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-//elements needed
+//Elements needed
 var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var questionsEl = document.querySelector("#questions");
 var choicesEl = document.querySelector("#choices");
 var initialsEl = document.querySelector("#initials");
 
-//Questions
+//Quiz Questions
 var questions = [
-  { title: "What", choices: ["1", "2", "3", "4"], answer: "1" },
-  { title: "What", choices: ["1", "2", "3", "4"], answer: "1" },
+  {
+    title: "Commonly used date type DO NOT include:",
+    choices: ["booleans", "strings", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if/else statment is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
   { title: "What", choices: ["1", "2", "3", "4"], answer: "1" },
 ];
+
+//Functions of the quiz
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-quiz");
+  startScreenEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
+  timerId = setInterval(clockTick, 100);
+  timerEl.textcontent = time;
+  getQuestion();
+}
